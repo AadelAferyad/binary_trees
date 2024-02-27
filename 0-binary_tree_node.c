35bc9bd181ddb98b52b20d9e923038b51d25bf1d
AadelAferyad <animevineu@gmail.com>
2024-02-27 16:14:20 +0100

    check if parent is null and set it self to be the parent?

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,13 +13,38 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent)
+	{
+		if (parent->left && parent->right)
+			return (NULL);
+	}
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
 	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 	if (parent)
 		node->parent = parent;
 	else
 		node->parent = node;
 	return (node);
+}
+
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+
+    root->left = binary_tree_node(root, 12);
+    root->left->left = binary_tree_node(root->left, 6);
+    root->left->right = binary_tree_node(root->left, 16);
+
+    root->right = binary_tree_node(root, 402);
+    root->right->left = binary_tree_node(root->right, 256);
+    root->right->right = binary_tree_node(root->right, 512);
+
+    binary_tree_print(root);
+    return (0);
 }
