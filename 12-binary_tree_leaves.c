84c586bca062b9a97e01f15fc28b299da69a0575
AadelAferyad <animevineu@gmail.com>
2024-02-28 15:57:09 +0100

    count leaves fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,5 +14,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 	left = binary_tree_leaves(tree->left);
 	right = binary_tree_leaves(tree->right);
-	return (binary_tree_is_leaf(tree) ? left + right + 1 : left + right);
+	if (tree->parent)
+	{
+		if (!tree->left && !tree->right)
+			return (left + right + 1);
+	}
+	return (left + right);
 }
