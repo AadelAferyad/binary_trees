64c87389e968f783a38a79b45902aca2dddd0394
AadelAferyad <animevineu@gmail.com>
2024-02-29 13:33:32 +0100

    is this binary tree perfect?

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -48,8 +48,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
+	size_t left = 0, right = 0;
 
+	if (!tree)
+		return (0);
 	left = height(tree->left);
 	right = height(tree->right);
 	return (binary_tree_is_full(tree) && left == right);
