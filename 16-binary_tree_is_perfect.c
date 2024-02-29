c2dba7d1495722a0ee4d6cf2a70d78b0dabba4b2
AadelAferyad <animevineu@gmail.com>
2024-02-29 13:44:25 +0100

    same height and same numbers of nodes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -20,23 +20,18 @@ size_t height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_full -  checks if a binary tree is full
+ * binary_tree_count_nodes -  counts all nodes in binary tree
  * @tree: the tree
- * Return: 1 if full other ways 0
+ * Return: counts other ways 0
  */
 
 
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_count_nodes(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-	left = binary_tree_is_full(tree->left);
-	right = binary_tree_is_full(tree->right);
-	return ((tree->left && tree->right) && left && right);
+	return (1 + binary_tree_count_nodes(tree->left) +
+	binary_tree_count_nodes(tree->right));
 }
 
 /**
@@ -54,5 +49,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	left = height(tree->left);
 	right = height(tree->right);
-	return (binary_tree_is_full(tree) && left == right);
+	return (binary_tree_count_nodes(tree->left) ==
+	binary_tree_count_nodes(tree->right) && left == right);
 }
